de912c2e7f69fe6fb8c0b5105ce5f4f28f779445
Eugene Sia <eugene@eugenesia.co.uk>
2020-05-15 00:44:30 +0100

    pom.xml: Target Java 11

--- src/main/java/net/eugenesia/eugenebot/App.java
+++ src/main/java/net/eugenesia/eugenebot/App.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 public class App {
   public static void main(String[] args) {
+    System.out.println("We're goinna run Cleverbot");
     CleverBotQuery bot = new CleverBotQuery("YOURAPIKEY", "someInputHere");
 
     String response;
